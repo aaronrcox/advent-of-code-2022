b4c5e403c1455bbed13c5e70b2ee500f076345dc
Aaron Cox <aaron.cox@aie.edu.au>
2022-12-06 17:48:35 +1100

    added aoc day 6

---
+++ 06-tuning-trouble/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace tuning_trouble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = File.ReadAllText("input.txt");
+
+            for(int i = 4; i < line.Length; i++)
+            {
+                // get last 4 characters
+                string code = line.Substring(i - 4, 4);
+                if (code.ToCharArray().Distinct().Count() == code.Length)
+                {
+                    Console.WriteLine(i + "\t Found Marker Pos");
+                    break;
+                }
+            }
+
+            for (int i = 14; i < line.Length; i++)
+            {
+                // get last 4 characters
+                string code = line.Substring(i - 14, 14);
+                if (code.ToCharArray().Distinct().Count() == code.Length)
+                {
+                    Console.WriteLine(i + "\t Found Message Pos");
+                    break;
+                }
+            }
+
+
+            Console.WriteLine("Program End");
+        }
+    }
+}
